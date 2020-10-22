6c65c015c5368af56e6935c68d90f06e25ab8a22
Arnab Sen <arnab.sen210@gmail.com>
2020-10-22 17:44:53 +1300

    New programming paradigm: IEdge

---
+++ ALACore/ProgrammingParadigms/IEdge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingParadigms
+{
+    public interface IEdge
+    {
+        object Source { get; set; }
+        object Destination { get; set; }
+    }
+}
