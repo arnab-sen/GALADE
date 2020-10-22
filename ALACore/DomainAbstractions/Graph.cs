8114d0afb92d2d27c7b59b196d040088dd0f7499
Arnab Sen <arnab.sen210@gmail.com>
2020-10-22 17:07:08 +1300

    New domain abstraction: Graph

---
+++ ALACore/DomainAbstractions/Graph.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libraries;
+using ProgrammingParadigms;
+
+namespace DomainAbstractions
+{
+    public class Graph : IGraph
+    {
+        // Public fields and properties
+        public string InstanceName { get; set; } = "Default";
+
+        // Private fields
+        private Dictionary<string, object> _nodeDictionary = new Dictionary<string, object>();
+        private Dictionary<string, object> _edgeDictionary = new Dictionary<string, object>();
+        private HashSet<string> _nodeIds = new HashSet<string>();
+        private HashSet<string> _edgeIds = new HashSet<string>();
+
+        // Ports
+
+        // IGraph implementation
+        HashSet<string> IGraph.NodeIds => _nodeDictionary.Keys.ToHashSet();
+
+        HashSet<string> IGraph.EdgeIds => _edgeDictionary.Keys.ToHashSet();
+
+        public void AddNode(string id, object node)
+        {
+            if (!ContainsNode(id))
+            {
+                _nodeDictionary[id] = node;
+            }
+            else
+            {
+                throw new Exception($"Error in AddNode: Graph already contains a node with id {id}.");
+            }
+        }
+        public bool ContainsNode(string id) => _nodeDictionary.ContainsKey(id);
+        public object GetNode(string id) => ContainsNode(id)  ? _nodeDictionary[id] : null;
+        public void DeleteNode(string id)
+        {
+            if (ContainsNode(id))
+            {
+                _nodeDictionary.Remove(id);
+            }
+            else
+            {
+                throw new Exception($"Error in DeleteNode: Graph does not contain a node with id {id}.");
+            }
+        }
+
+        public void AddEdge(string id, object edge)
+        {
+            if (!ContainsEdge(id))
+            {
+                _edgeDictionary[id] = edge;
+            }
+            else
+            {
+                throw new Exception($"Error in AddEdge: Graph already contains an edge with id {id}.");
+            }
+        }
+
+        public bool ContainsEdge(string id) => _edgeDictionary.ContainsKey(id);
+
+        public object GetEdge(string id) => ContainsEdge(id) ? _edgeDictionary[id] : null;
+
+        public void DeleteEdge(string id)
+        {
+            if (ContainsEdge(id))
+            {
+                _edgeDictionary.Remove(id);
+            }
+            else
+            {
+                throw new Exception($"Error in DeleteEdge: Graph does not contain an edge with id {id}.");
+            }
+        }
+
+
+        // Methods
+
+
+        public Graph()
+        {
+
+        }
+    }
+}
--- ALACore/ProgrammingParadigms/IGraph.cs
+++ ALACore/ProgrammingParadigms/IGraph.cs
@@ -13,19 +13,22 @@ namespace ProgrammingParadigms
     /// <para>It also has several operation functions, to add/get/delete nodes and edges.</para>
     /// <para>Each node and edge should have a unique associated string id.
     /// It is up to the class implementing IGraph to decide how it wants to store the objects themselves.
-    /// A simple Dictionary&lt;string, object&gt; is recommended, although not enforced, to allow for the objects to be cast before being stored.</para>
+    /// A simple Dictionary&lt;string, object&gt; is recommended, although not enforced, to allow for the objects to be processed
+    /// in some way (e.g. type casting or sorting) before being stored.</para>
     /// </summary>
     public interface IGraph
     {
-        HashSet<string> NodeIds { get; set; }
-        HashSet<string> EdgeIds { get; set; }
+        HashSet<string> NodeIds { get; }
+        HashSet<string> EdgeIds { get; }
 
         void AddNode(string id, object node);
+        bool ContainsNode(string id);
         object GetNode(string id);
-        void DeleteNode(string id, object node);
+        void DeleteNode(string id);
 
         void AddEdge(string id, object edge);
+        bool ContainsEdge(string id);
         object GetEdge(string id);
-        void DeleteEdge(string id, object edge);
+        void DeleteEdge(string id);
     }
 }
