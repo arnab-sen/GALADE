20f71a1108d1a08c2fcef1ed8ab254a34772b150
Arnab Sen <arnab.sen210@gmail.com>
2020-10-23 14:12:10 +1300

    New programming paradigm: IAttachable

--- ALACore/ProgrammingParadigms/IAttachable.cs
+++ ALACore/ProgrammingParadigms/IAttachable.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Libraries;
 
 namespace ProgrammingParadigms
 {
     /// <summary>
     /// Represents an item that can be attached at a point to another IAttachable.
+    /// When two items are attached, movement of a source should try to move its children.
     /// </summary>
     public interface IAttachable
     {
-        List<Tuple<IAttachable, Point>> Attachments { get; set; }
-        void Attach(IAttachable item, Point point = default);
+        Canvas Canvas { get; set; }
+        List<IAttachable> AttachedItems { get; }
+        void Attach(IAttachable candidate); // Let the source decide the position
+        void Attach(IAttachable candidate, Point location); // Let the candidate suggest a position
+        Point FindNearestAttachmentPoint(Point candidatePoint);
+        void Displace(double xDelta, double yDelta);
+        void MoveTo(Point point);
     }
 }
