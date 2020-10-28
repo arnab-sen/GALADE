2dd3ca2284b98085c43798dbebecd43926fa7804
Arnab Sen <arnab.sen210@gmail.com>
2020-10-29 09:04:48 +1300

    New domain abstraction: BezierCurve

---
+++ ALACore/DomainAbstractions/BezierCurve.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Libraries;
+using ProgrammingParadigms;
+
+namespace DomainAbstractions
+{
+    public class BezierCurve : IUI
+    {
+        // Public fields and properties
+        public string InstanceName { get; set; } = "Default";
+
+        public Brush Colour
+        {
+            get => _path.Stroke;
+            set => _path.Stroke = value;
+        }
+
+        public double StrokeThickness
+        {
+            get => _path.StrokeThickness;
+            set => _path.StrokeThickness = value;
+        }
+
+        public Point Point1
+        {
+            get => _point1;
+            set
+            {
+                _point1 = value;
+                Update();
+            }
+        }
+
+        public Point Point2
+        {
+            get => _point2;
+            set
+            {
+                _point2 = value;
+                Update();
+            }
+        }
+
+        public Point Point3
+        {
+            get => _point3;
+            set
+            {
+                _point3 = value;
+                Update();
+            }
+        }
+
+        public UIElement Render => _path;
+
+        // Private fields
+        private Point _point1 = new Point(0, 0);
+        private Point _point2 = new Point(0, 0);
+        private Point _point3 = new Point(0, 0);
+        private Path _path = new Path();
+        private BezierSegment _bezier = new BezierSegment();
+
+        // Ports
+        private List<IEventHandler> eventHandlers = new List<IEventHandler>();
+
+        // IUI implementation
+        UIElement IUI.GetWPFElement()
+        {
+            _bezier = new BezierSegment()
+            {
+                Point1 = Point1,
+                Point2 = Point2,
+                Point3 = Point3
+            };
+
+            _path = new Path()
+            {
+                Focusable = true,
+                FocusVisualStyle = null,
+                Stroke = Colour,
+                StrokeThickness = StrokeThickness,
+                Data = new PathGeometry()
+                {
+                    Figures = new PathFigureCollection()
+                    {
+                        new PathFigure()
+                        {
+                            Segments = new PathSegmentCollection()
+                            {
+                                _bezier
+                            }
+                        }
+                    }
+                }
+            };
+
+            foreach (var eventHandler in eventHandlers)
+            {
+                eventHandler.Sender = this;
+            }
+
+            return _path;
+        }
+
+        // Methods
+        private void Update()
+        {
+            _bezier.Point1 = Point1;
+            _bezier.Point2 = Point2;
+            _bezier.Point3 = Point3;
+        }
+
+        public List<Point> GetPoints() => new List<Point>() { Point1, Point2, Point3 };
+
+        public BezierCurve()
+        {
+
+        }
+    }
+}
---
+++ ALACore/RequirementsAbstractions/ALAWire.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Libraries;
+using ProgrammingParadigms;
+using DomainAbstractions;
+
+namespace RequirementsAbstractions
+{
+    public class ALAWire
+    {
+        // Public fields and properties
+        public string InstanceName { get; set; } = "Default";
+        public string Label { get; set; } = "";
+        public Graph Graph { get; set; }
+        public Canvas Canvas { get; set; }
+        public UIElement Render { get; set; }
+        public Port SourcePort { get; set; }
+        public Port DestinationPort { get; set; }
+
+        // Private fields
+        private Box rootUI;
+
+        // Ports
+
+        // Methods
+        private void SetWiring()
+        {
+            rootUI = new Box() { Background = Brushes.Transparent };
+
+            // BEGIN AUTO-GENERATED INSTANTIATIONS
+            // END AUTO-GENERATED INSTANTIATIONS
+
+            // BEGIN AUTO-GENERATED WIRING
+            // END AUTO-GENERATED WIRING
+
+            Render = (rootUI as IUI).GetWPFElement();
+        }
+
+        private AbstractionModel CreateDummyAbstractionModel()
+        {
+            var model = new AbstractionModel();
+            model.AddImplementedPort("IEvent", "input1");
+            model.AddImplementedPort("IEvent", "input2");
+            model.AddImplementedPort("IEvent", "input3");
+            model.AddAcceptedPort("IEvent", "complete");
+            model.AddProperty("Type", "Box");
+            model.AddProperty("Name", "test");
+
+            return model;
+        }
+
+        public void CreateInternals()
+        {
+            SetWiring();
+        }
+
+        public ALAWire()
+        {
+
+        }
+    }
+}
