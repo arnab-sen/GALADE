274f35d0dbaf105d9aff189ce7d42fec93211d85
Arnab Sen <arnab.sen210@gmail.com>
2020-12-23 13:09:13 +1300

    Updated to v1.6.1

--- GALADE_Standalone/Application/Application.cs
+++ GALADE_Standalone/Application/Application.cs
@@ -87,7 +87,7 @@ namespace Application
 
         private void CreateWiring()
         {
-            var VERSION_NUMBER = "1.6.0";
+            var VERSION_NUMBER = "1.6.1";
 
             #region Set up directory and file paths
             string APP_DIRECTORY = Utilities.GetApplicationDirectory();
