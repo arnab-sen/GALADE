11b8679d3b05b37e27127a7b4247ff12f4b217a5
Arnab Sen <arnab.sen210@gmail.com>
2021-01-06 19:45:02 +1300

    New domain abstraction: FlowGate

---
+++ ALACore/DomainAbstractions/FlowGate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libraries;
+using ProgrammingParadigms;
+
+namespace DomainAbstractions
+{
+    /// <summary>
+    /// <para>A gate that blocks a data or event flow based on an input boolean.</para>
+    /// <para>Ports:</para>
+    /// <para>1. IDataFlow&lt;T&gt; inputData: The data flow.</para>
+    /// <para>2. IEvent inputEvent: The event flow.</para>
+    /// <para>3. IDataFlowB&lt;bool&gt; isOpen: Set false to block the data and event flow, and true to let them pass through.</para>
+    /// </summary>
+    public class FlowGate<T> : IDataFlow<T>, IEvent // inputData, inputEvent
+    {
+        // Public fields and properties
+        public string InstanceName { get; set; } = "Default";
+        public string InstanceDescription { get; set; } = "";
+
+        // Private fields
+        private T _data;
+        private bool _isOpen = false;
+        private bool _hasData = false;
+        private bool _hasEvent = false;
+
+        // Ports
+        private IDataFlowB<bool> isOpen;
+        private IDataFlow<T> dataOutput;
+        private IEvent eventOutput;
+
+        // Methods
+        public void SendData()
+        {
+            if (dataOutput != null) dataOutput.Data = _data;
+            _hasData = false;
+        }
+
+        public void SendEvent()
+        {
+            eventOutput?.Execute();
+        }
+
+        // IDataFlow<T> implementation
+        T IDataFlow<T>.Data
+        {
+            get => _data;
+            set
+            {
+                _data = value;
+                if (_isOpen)
+                {
+                    SendData();
+                }
+                else
+                {
+                    _hasData = true;
+                }
+            }
+        }
+
+        // IEvent implementation
+        void IEvent.Execute()
+        {
+            if (_isOpen)
+            {
+                SendEvent();
+            }
+            else
+            {
+                _hasEvent = true;
+            }
+        }
+
+        private void PostWiringInitialize()
+        {
+            if (isOpen != null) isOpen.DataChanged += () =>
+            {
+                _isOpen = isOpen.Data;
+                if (_isOpen)
+                {
+                    if (_hasData) SendData();
+                    if (_hasEvent) SendEvent();
+                }
+            };
+        }
+
+        public FlowGate()
+        {
+
+        }
+    }
+}
